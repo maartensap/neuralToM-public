f852363c26ba361ee9cfaedb2bbf38d32ef78555
XuhuiZhou <zhouxuhui2018@gmail.com>
2023-03-24 14:20:16 -0400

    💄 seperate .sh file argument

--- exampleSIQAcommand.sh
+++ exampleSIQAcommand.sh
@@ -1,1 +1,6 @@
-python gpt3LMMCprobing.py --task siqa --debug_dev 200 --n_examples 0 --model_variant gpt-3.5-turbo-0301 --probing_type mc --group_accuracy_by reasoningDim promptDim promptQuestionFocusChar
+python gpt3LMMCprobing.py --task siqa \
+    --debug_dev 200 \
+    --n_examples 0 \
+    --model_variant gpt-3.5-turbo-0301 \
+    --probing_type mc \
+    --group_accuracy_by reasoningDim promptDim promptQuestionFocusChar
