0f512285994b792265d82b2fd6df623993dd9c33
XuhuiZhou <zhouxuhui2018@gmail.com>
2023-03-28 10:02:05 -0400

    🙈 save prediction file

--- exampleSIQAcommand.sh
+++ exampleSIQAcommand.sh
@@ -1,6 +1,7 @@
 python gpt3LMMCprobing.py --task siqa \
-    --debug_dev 200 \
+    --debug_dev 50 \
     --n_examples 0 \
     --model_variant gpt-3.5-turbo-0301 \
-    --probing_type mc \
-    --group_accuracy_by reasoningDim promptDim promptQuestionFocusChar
+    --probing_type nli \
+    --group_accuracy_by reasoningDim promptDim promptQuestionFocusChar \
+    --output_prediction_file predictions/siqa_gpt3.5-turbo-0301_nli.csv \
